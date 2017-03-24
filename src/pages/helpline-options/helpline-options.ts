a99ffa319cb9c84be164204a2fdc59472141f0e2
ignisit <ignisit@ignisits-Mac-mini.local>
2017-03-24 14:41:04 +0530

    Fetched data from db

--- src/pages/helpline-options/helpline-options.ts
+++ src/pages/helpline-options/helpline-options.ts
@@ -17,10 +17,15 @@ import { HelplineDetailPage } from '../helpline-detail/helpline-detail';
   export class HelplineOptionsPage {
     helplineDetailPage = HelplineDetailPage;
 
+    helplineDataUSACANADA = [];
+    helplineDataCountry = [];
+
     constructor(public navCtrl: NavController, public navParams: NavParams, private sqliteData:SqliteData, public common:Common, public plt:Platform) {}
 
     ionViewDidLoad() {
       console.log('ionViewDidLoad HelplineOptionsPage');
+
+      
 
       this.sqliteData.getHelplinesDetails().then(res => {
         console.log("Helpline Data"+JSON.stringify(res));
@@ -35,7 +40,7 @@ import { HelplineDetailPage } from '../helpline-detail/helpline-detail';
             if (ds.length > 0) {
               for (var i = 0; i < ds.length; i++) {
                 var row = ds.item(i);
-                // $('#cg_usa_states').append('<div  data-corners="true" data-shadow="true" data-iconshadow="true" data-wrapperels="span" data-icon="arrow-r" data-iconpos="right" data-theme="a" data-disabled="false" class="ui-btn ui-shadow ui-btn-corner-all ui-btn-icon-right ui-btn-up-a" aria-disabled="false"><span class="ui-btn-inner"><span class="ui-btn-text">[USA] ' + row['statename'] + '</span><span class="ui-icon ui-icon-arrow-r ui-icon-shadow">&nbsp;</span></span><input onClick="helplineOptions(this);" type="button" value="[USA] ' + row['statename'] + '" data-iconpos="right" data-icon="arrow-r" class="btnUSAState ui-btn-hidden" data-disabled="false"></div>');
+                this.helplineDataUSACANADA.push("[USA] "+row['statename']);
               }
             }
           }, error => {
@@ -43,13 +48,13 @@ import { HelplineDetailPage } from '../helpline-detail/helpline-detail';
           });
 
           // Fill in Canadian Provinces
-          this.sqliteData.getStateNameOfUSAFromHelplines().then(result => {
+          this.sqliteData.getStateNameOfCanadaFromHelplines().then(result => {
             console.log("Canada Helplines "+JSON.stringify(result));
             var ds = result['rows'];
             if (ds.length > 0) {
               for (var i = 0; i < ds.length; i++) {
                 var row = ds.item(i);
-                // $('#cg_usa_states').append('<div id="test" data-corners="true" data-shadow="true" data-iconshadow="true" data-wrapperels="span" data-icon="arrow-r" data-iconpos="right" data-theme="a" data-disabled="false" class="ui-btn ui-shadow ui-btn-corner-all ui-btn-icon-right ui-btn-up-a" aria-disabled="false"><span class="ui-btn-inner"><span class="ui-btn-text">[Canada] ' + row['statename'] + '</span><span class="ui-icon ui-icon-arrow-r ui-icon-shadow">&nbsp;</span></span><input onClick="helplineOptions(this);" type="button" value="[Canada] ' + row['statename'] + '" data-iconpos="right" data-icon="arrow-r" class="btnUSAState ui-btn-hidden" data-disabled="false"></div>');
+                this.helplineDataUSACANADA.push("[Canada] "+row['statename']);               
               }
             }
           }, error => {
@@ -63,7 +68,7 @@ import { HelplineDetailPage } from '../helpline-detail/helpline-detail';
             if (ds.length > 0) {
               for (var i = 0; i < ds.length; i++) {
                 var row = ds.item(i);
-                // $('#cg_countries').append('<div data-corners="true" data-shadow="true" data-iconshadow="true" data-wrapperels="span" data-icon="arrow-r" data-iconpos="right" data-theme="a" data-disabled="false" class="ui-btn ui-shadow ui-btn-corner-all ui-btn-icon-right ui-btn-up-a" aria-disabled="false"><span class="ui-btn-inner"><span class="ui-btn-text">' + row['countryname'] + '</span><span class="ui-icon ui-icon-arrow-r ui-icon-shadow">&nbsp;</span></span><input onClick="helplineOptionsCountry(this);" type="button" value="' + row['countryname'] + '" data-iconpos="right" data-icon="arrow-r" class="btnCountry ui-btn-hidden" data-disabled="false"></div>');
+                this.helplineDataCountry.push(row['countryname']);
               }
             }
           }, error => {
@@ -76,5 +81,13 @@ import { HelplineDetailPage } from '../helpline-detail/helpline-detail';
       }, error => {
         console.error(error);
       });
+    }
+
+    usaWorldChange() {
+
+    }
+    
+    areaSearchCode() {
+
     }
   }
